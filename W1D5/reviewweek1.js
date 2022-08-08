ce577f1e148d791ae68e8c8b108f19e19c5d35de
Cornell Bethea <betheacornellb@gmail.com>
2022-08-08 01:48:51 -0400

    revieweek1

--- W1D5/reviewweek1.js
+++ W1D5/reviewweek1.js
@@ -38,17 +38,17 @@
 // luckySevens(25); // => [ 7, 14, 21 ]
 // luckySevens(42); // => [ 7, 14, 21, 28, 35, 42 ]
 
-function luckySevens(max) {
-    let arr = []
-    for (i = 0; i <= max; i++) {
-        if (i % 7 === 0) {
-        arr.push(i)
-        }
-    }
-    return arr
-}
+// function luckySevens(max) {
+//     let arr = []
+//     for (i = 0; i <= max; i++) {
+//         if (i % 7 === 0) {
+//         arr.push(i)
+//         }
+//     }
+//     return arr
+// }
 
-console.log(luckySevens(25))
+// console.log(luckySevens(25))
 
 // Write a function `copyMachine(element, num)` that takes in an element and a number
 // it should return an array of length `num` that is filled with `element`.
@@ -60,13 +60,99 @@ console.log(luckySevens(25))
 // copyMachine('bread', 4); // => [ 'bread', 'bread', 'bread', 'bread' ]
 
 
-function copyMachine(ele, num) {
-    let arr = []
-    for (let i = 0; i < num; i++) {
-        arr.push(ele)
+// function copyMachine(ele, num) {
+//     let arr = []
+//     for (let i = 0; i < num; i++) {
+//         arr.push(ele)
+//     }
+//     return arr
+// }
+// //
+
+// console.log(copyMachine(11, 6)); // => [ 11, 11, 11, 11, 11, 11 ]
+
+
+// Write a function `everyOtherWord(sentence)` that takes in a sentence and returns
+// an array containing every other word in that sentence.
+//
+// Examples:
+//
+// everyOtherWord('the weather is wonderful'); // => [ 'the', 'is' ]
+
+// function everyOtherWord(sentence) {
+//     arr = []
+//     newSen = sentence.split(' ')
+//     for (let i = 0; i <= newSen.length; i += 2) {
+//         word = newSen[i]
+//         arr.push(word)
+//     }
+//     return arr
+// }
+
+// console.log(everyOtherWord('hello how are you doing on this lovely day?')); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
+
+// function wordYeller(sentence) {
+//     yellerWords = []
+
+//     let spl = sentence.split(" ")
+//     let punctuation = ". , ! ? ; :".split(" ")
+
+//     for (let i = 0; i < spl.length; i++); {
+//         let word = spl[i];
+//         let lastChar = word.slice(-1);
+
+//      if (punctuation.indexOf(lastChar) !== -1) { 
+//         yellerWords.push(word);
+//       } else {
+//          yellerWords.push(word + "!");
+//       }
+//     }
+//     return yellerWords.join(" ")
+// }
+
+
+// var sent = "Stop it now! Please, wont you stop?";
+// var yelledWords = wordYeller(sent);
+// yelledWords === "Stop! it! now! Please, wont! you! stop?"; // => true
+
+
+// Write a function `wordYeller(sentence)` that takes in a sentence string as
+// input. It should return the sentence where every word has an exclamation
+// point behind it, except words that already have punctuation. Punctuation
+// marks are ". , ! ? ; :"
+//
+// Example 1
+//
+// Example 2
+// var words = "Go to the store and grab the following: milk, bread, run, and cake";
+// wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! 
+
+
+// Write a function `arraySubstring(words, str)` that takes in an array of
+// strings and a string as arguments and returns an array where each element is
+// replaced with true if the string is a substring of the element and false if
+// it is not.
+//
+// Examples:
+//
+// => [true, false, true, true]
+// arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre")
+// => [true, false, false, true]
+
+function arraySubstring(word, str) {
+    let arr = [];
+
+    for (let i = 0; i < word.length; i++) {
+        let num = word[i];
+
+        if (num.indexOf(str) !== -1) {
+        arr.push(true);
+    } else {
+        arr.push(false);
     }
+}
     return arr
 }
-//
 
-console.log(copyMachine(11, 6)); // => [ 11, 11, 11, 11, 11, 11 ]
+
+console.log(arraySubstring(["hello", "history", "helix", "hellos"],"hel"))
