1b6169c1d408f1cee2b21bd3988562495b986c27
Cornell Bethea <betheacornellb@gmail.com>
2022-08-09 00:44:59 -0400

    W2D1ps

---
+++ w2d1_pset/problems/problems/5_first_last_cap.js
@@ -0,0 +1,33 @@
+/***********************************************************************
+Write a function `firstLastCap(sentence)` that takes in a sentence and
+returns a new sentence where the first and last letters of each word is
+uppercase. All other characters should be lowercase.
+
+Examples:
+
+firstLastCap('hello BOOTCAMP PrEp'); // =>'HellO BootcamP PreP'
+firstLastCap('what is on the radio'); // =>'WhaT IS ON ThE RadiO'
+***********************************************************************/
+
+function firstLastCap(sentence) {
+    let sen = sentence.split(' ');
+    let arr = []
+
+    for(let i = 0; i < sen.length; i++) {
+        let word = sen[i]
+        let newWord = word.toLowerCase();
+        let firstChar = newWord[0].toUpperCase();
+        let lastChar = newWord[newWord.length - 1].toUpperCase();
+        newWord1 = firstChar + newWord.slice(1,-1)+ lastChar;
+        arr.push(newWord1)
+    }
+    let newSen = arr.join(' ')
+    return newSen
+
+} 
+
+console.log(firstLastCap('hello BOOTCAMP PrEp'))
+console.log()
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = firstLastCap;
